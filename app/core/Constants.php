46931a2006115a7b26e4980737646fdfb81a40c4
MalikiPKL <90675926+MalikiPKL@users.noreply.github.com>
2022-01-18 10:18:06 +0700

    video 7

--- app/core/Constants.php
+++
@@ -1,2 +0,0 @@
-<?php
-define("BASEURL","http://localhost/tugas-1/public");
---
+++ app/models/Siswa_model.php
@@ -0,0 +1,20 @@
+<?php
+class Siswa_model
+{
+    private $table = "siswa";
+    private $db;
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+    public function getAllSiswa()
+    {
+        $this->db->query("SELECT * FROM $this->table");
+        return $this->db->resultSet();
+    }
+    public function getSiswaById($id) {
+        $this->db->query("SELECT * FROM $this->table WHERE id=:id");
+        $this->db->bind("id",$id);
+        return $this->db->sigle();
+    }
+}
